dfcf90a662eec45b86ed11b7b4400480ca354e83
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-05-23 17:50:42 -0400

    Subiendo Archivos

--- PrimerRegistro/Startup.cs
+++ PrimerRegistro/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PrimerRegistro.Data;
+
 
 namespace PrimerRegistro
 {
@@ -29,6 +31,7 @@ namespace PrimerRegistro
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
